ff9976a343db856eacdf07a1869580cf4131e002
rwg070607 <a56554696a@gmail.com>
2024-05-11 01:53:08 +0900

    2nd commit

---
+++ my_app/_index.js
@@ -0,0 +1,11 @@
+import {createServer} from 'http';
+
+const server = createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('hello world');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is working');
+});
